f177a99d4face8355c3c92ecb16c7e0ae48733a7
Muhammed Afees <afeesmuhammad703@gmail.com>
2024-03-28 18:38:31 +0530

    session created

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path')
+const session = require('express-session')
 const userRouter = require('./routes/userRoutes');
 const { connection } = require('./connection/connection');
-
-
+const uuid = require('uuid')
+const randomSectret = uuid.v4();
+console.log(randomSectret)
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+app.use(session({
+    secret:randomSectret,
+    resave:false,
+    saveUninitialized:true
+}))
 app.set('view engine','ejs');
 app.use("/public", express.static(path.join(__dirname, "public")));
 //mongodb connection
